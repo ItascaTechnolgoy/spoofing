090e2551d37cbf2e2de22b223b392f4d4905536d
Itasca Technologies <info@itasca.co.in>
2025-08-18 18:19:59 +0530

    First springboot commit

---
+++ src/main/java/com/itasca/spoofing/SpoofingApplication.java
@@ -0,0 +1,13 @@
+package com.itasca.spoofing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoofingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpoofingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itasca/spoofing/SpoofingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itasca.spoofing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoofingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
